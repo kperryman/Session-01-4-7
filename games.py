5b5391e88830b3f4d370629b3e8d3723859884f1
kperryman <101473071+kperryman@users.noreply.github.com>
2022-03-30 11:06:43 -0700

    Add files via upload
    Session 01 - problems 4-6

---
+++ games.py
@@ -0,0 +1,21 @@
+game1=13
+game2=18
+game3=21
+game4=17
+game5=31
+ave=(game1+game2+game3+game4+game5)/5
+print("Diana's games", game1, game2, game3, game4, game5)
+print("Diana's average", ave)
+print("Enter your game scores:")
+G1=input("Game1:")
+G2=input("Game2:")
+G3=input("Game3:")
+G4=input("Game4:")
+G5=input("Game5:")
+G1=int(G1)
+G2=int(G2)
+G3=int(G3)
+G4=int(G4)
+G5=int(G5)
+Total=(G1+G2+G3+G4+G5)
+print("Total score:", (Total), "Average score:", (Total/5))
